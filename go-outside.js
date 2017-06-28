b58f056af7a0b8cb8df6c41206232946651c68e9
Isaac Vallee <isaacvallee@gmail.com>
2017-06-28 17:03:37 +0000

    Adds conditionals & Ice Cream exercises

---
+++ go-outside.js
@@ -0,0 +1,25 @@
+var raining = true;
+var temperature = 5;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Probably need a sweater.");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+
+/*
+if (raining && cold) {
+  console.log("It's gross and cold outside! Bring all the things!")
+} else {
+    console.log("Now you're ready to go outside!");
+}
+*/
+
+
