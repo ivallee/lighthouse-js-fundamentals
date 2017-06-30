cb9e8eeaa668e4f7400d9bb190d25586c08155b1
Isaac Vallee <isaacvallee@gmail.com>
2017-06-30 17:55:37 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("Errabody now!");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
