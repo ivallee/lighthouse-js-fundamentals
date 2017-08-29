b0f4bd8c890e811d60d88c2da93631c9ac412df1
Isaac Vallee <isaacvallee@gmail.com>
2017-08-29 21:50:21 +0000

    adds more JS fundamental exercises round 2

---
+++ area-calc-2.js
@@ -0,0 +1,38 @@
+function calculateRectangleArea(x, y) {
+  var result = x * y;
+  if (x < 0 || y < 0) {
+    return undefined;
+  } else {
+    return result;
+  }
+}
+
+function  calculateTriangleArea(base, height) {
+  var result = base * height / 2;
+  if (base < 0 || height < 0) {
+    return undefined;
+  } else {
+    return result;
+  }
+}
+
+function calculateCircleArea(radius) {
+  var result = Math.PI * Math.pow(radius, 2);
+  if (radius < 0) {
+    return undefined;
+  } else {
+    return result;
+  }
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
